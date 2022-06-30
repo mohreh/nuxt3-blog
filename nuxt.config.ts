914b72bb368657ecb86257fbd0f51fb78290f90c
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-07-01 01:27:57 +0430

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  meta: {
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  css: ['~/assests/css/tailwindcss.css'],
+  modules: ['@nuxtjs/tailwindcss'],
+});
