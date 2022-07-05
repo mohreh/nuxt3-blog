346957e23d3a647d7608fe8444720d64be899536
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-07-05 16:15:13 +0430

    prisma mirgrate

--- prisma/migrations/20220705102657_init/migration.sql
+++ prisma/migrations/20220705102657_init/migration.sql
@@ -4,13 +4,12 @@ CREATE TYPE "Role" AS ENUM ('User', 'Admin');
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "verified" BOOLEAN NOT NULL DEFAULT false,
-    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'User',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
