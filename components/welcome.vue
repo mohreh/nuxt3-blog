d870ecc470c75c9b72c8af2e7e3dfe194c7aa0be
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-07-09 05:46:15 +0430

    invite to register message, structure of main page

---
+++ components/welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-slate-700 dark:text-slate-300/90">
+    <h3 class="text-xl font-bold">Weolcom to nuxtblog!</h3>
+    <p>start writing about your favorate topics and share ideas.</p>
+    <nuxt-link to="/login-register" class="group link max-w-fit">
+      <p class="font-semibold">create account</p>
+      <ChevronRightIcon class="w-4 mt-1 group-hover:ml-1" />
+    </nuxt-link>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ChevronRightIcon } from '@heroicons/vue/outline';
+</script>
+
+<style lang="postcss" scoped>
+.link {
+  @apply flex flex-row items-center hover:text-slate-900 hover:dark:text-slate-200;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>Main Page</div>
+  <div class="flex flex-row space-x-4 h-full mt-5">
+    <section class="flex-none w-64 space-y-4">
+      <welcome v-if="!userStore.username" />
+
+      <div class="bordered p-2">
+        <h3>Famous Communities</h3>
+      </div>
+
+      <div class="bordered p-2">
+        <h3>Footer stuff</h3>
+      </div>
+    </section>
+
+    <section class="bordered grow">
+      <h3>Posts</h3>
+    </section>
+
+    <section class="flex-none w-64 space-y-4">
+      <div class="bordered p-2">
+        <h3>Top writers</h3>
+      </div>
+
+      <div class="bordered p-2">
+        <h3>Trends</h3>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { useUserStore } from '~~/store/user';
+
 definePageMeta({
   layout: 'default',
 });
+
+const userStore = useUserStore();
 </script>
+
+<style lang="postcss">
+section {
+  @apply h-full;
+}
+
+h3 {
+  @apply dark:text-white text-xl font-bold;
+}
+
+.bordered {
+  @apply border border-slate-900/10 dark:border-slate-50/[0.06] rounded-lg;
+}
+</style>
